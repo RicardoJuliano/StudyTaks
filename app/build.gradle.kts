fccd863258d0898894b687e33ffcc0072fd03fb8
Ricardo Juliano <rjulianojunior@gmail.com>
2026-08-10 18:05:25 -0300

    feat: build the tasks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.game.studytasks"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
--- app/src/main/java/com/game/studytasks/MainActivity.kt
+++ app/src/main/java/com/game/studytasks/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.game.studytasks
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.game.studytasks.ui.theme.StudyTasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +25,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             StudyTasksTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StudyTasksScreen(
+                        name = "Study Tasks",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -29,19 +34,46 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+data class StudyTask(
+    val id: Int,
+    val title: String,
+    val category: String,
+    val isDone: Boolean
+)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun StudyTasksScreen(name: String, modifier: Modifier = Modifier) {
+
+
+    val tasks = listOf(
+        StudyTask(1, "Review data classes", "Kotlin", false),
+        StudyTask(2, "Build a list with LazyColumn", "Jetpack Compose", false),
+        StudyTask(3, "Understand remember and mutableStateOf", "State and Recomposition", false),
+        StudyTask(4, "Move logic into a ViewModel", "Android Architecture", false),
+        StudyTask(5, "Save tasks with Room", "Local Persistence", false)
     )
+    Spacer(modifier = Modifier.height(25.dp))
+    Column( Modifier.padding(35.dp)) {
+        Text(text = "Study Tasks")
+
+        for (task in tasks) {
+            Text(text = "Task ${task.id}")
+            Text(text = task.title)
+            Text(text = "Language: ${task.category}")
+
+            if (task.isDone) {
+                Text(text = "Task Completed")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StudyTasksTheme {
-        Greeting("Android")
+        StudyTasksScreen("Android")
     }
 }
