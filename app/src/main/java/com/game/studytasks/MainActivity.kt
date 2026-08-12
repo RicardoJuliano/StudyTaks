79cf2d880788d6c672ed3eb44608940ff69c522e
Ricardo Juliano <rjulianojunior@gmail.com>
2026-08-12 20:12:56 -0300

    feat: add bottom navigation tabs

--- app/src/main/java/com/game/studytasks/MainActivity.kt
+++ app/src/main/java/com/game/studytasks/MainActivity.kt
@@ -5,16 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,6 +55,50 @@ data class StudyTask(
 @Composable
 fun StudyTasksScreen(name: String, modifier: Modifier = Modifier) {
 
+    var selectedScreen by remember { mutableStateOf("Tasks") }
+   
+
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = selectedScreen == "Tasks",
+                    onClick = { selectedScreen = "Tasks" },
+                    icon = { Text(text = "T") },
+                    label = { Text(text = "Tasks") }
+                )
+
+                NavigationBarItem(
+                    selected = selectedScreen == "Progress",
+                    onClick = { selectedScreen = "Progress" },
+                    icon = { Text(text = "P") },
+                    label = { Text(text = "Progress") }
+                )
+
+                NavigationBarItem(
+                    selected = selectedScreen == "Profile",
+                    onClick = { selectedScreen = "Profile" },
+                    icon = { Text(text = "U") },
+                    label = { Text(text = "Profile") }
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            when (selectedScreen) {
+                "Tasks" -> TasksContent()
+                "Progress" -> ProgressContent()
+                "Profile" -> Text(text = "Profile Screen")
+            }
+        }
+    }
+
+}
+@Composable
+fun TasksContent() {
 
     val tasks = listOf(
         StudyTask(1, "Review data classes", "Kotlin", false),
@@ -54,34 +107,39 @@ fun StudyTasksScreen(name: String, modifier: Modifier = Modifier) {
         StudyTask(4, "Move logic into a ViewModel", "Android Architecture", false),
         StudyTask(5, "Save tasks with Room", "Local Persistence", false)
     )
-    Spacer(modifier = Modifier.height(25.dp))
-    Column( Modifier.padding(35.dp)) {
-        Text(text = "Study Tasks")
 
-        Spacer(modifier = Modifier.height(20.dp))
+    LazyColumn(
+        modifier = Modifier.padding(40.dp),
+        verticalArrangement = Arrangement.spacedBy(9.dp)
+    ) {
 
+        item{  Text(text = "Study Tasks")}
 
-            for (task in tasks) {
-                Card(
-                    modifier = Modifier.fillMaxWidth()
+        items(tasks){ task ->
+            Card(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier.padding(17.dp)
                 ) {
-                    Column(
-                        modifier = Modifier.padding(17.dp)
-                    ) {
-                        Text(text = "Task ${task.id}")
-                        Text(text = task.title)
-                        Text(text = "Language: ${task.category}")
+                    Text(text = "Task ${task.id}")
+                    Text(text = task.title)
+                    Text(text = "Language: ${task.category}")
 
-                        if (task.isDone) {
-                            Text(text = "Task Completed")
-                        }
-
+                    if (task.isDone) {
+                        Text(text = "Task Completed")
                     }
+
                 }
-                Spacer(modifier = Modifier.height(28.dp))
+            }
+            Spacer(modifier = Modifier.height(28.dp))
         }
     }
 
+}
+@Composable
+fun ProgressContent(){
+    Text(text = "Porcentagem de progresso")
 }
 
 @Preview(showBackground = true)
